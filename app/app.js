3995abffeb76b48244dd12f1ce56da74ada2dd2c
louizzel <zelogalesco@gmail.com>
2014-12-07 04:22:31 +0800

    For smooth scrolling

--- app/app.js
+++ app/app.js
@@ -1,3 +1,5 @@
 // 'use strict';
 // Global variables
-var app = angular.module('TheLineProject', ['ngRoute', 'ngResource']);
+var app = angular.module('TheLineProject', ['ngRoute', 'ngResource', 'duScroll'])
+				 .value('duScrollOffset', 0)
+				 .value('duScrollDuration', 1500);
